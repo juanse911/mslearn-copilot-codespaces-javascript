c322449e8b5465d23aabac784d0d1c7a8fc755fe
juanse911 <juanrs911@hotmail.com>
2023-11-10 03:05:55 +0000

    ajustes en pagina precargada

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Juan Saavedra",
+  title: "Analissta de sopote senior",
+  email: "juanrs911@hotmail.com",
+  gitHub: "juanse911",
+  instagram: "juanse911",
+  linkedIn: "juansesaavedra911",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
